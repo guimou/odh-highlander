78a3ebe4a35da25b919a9fdf63b6a0855a08df01
Guillaume Moutier <guillaume.moutier@gmail.com>
2021-02-21 21:35:34 -0500

    build and notes

--- easybuild-vm/fedora33_base.sh
+++ easybuild-vm/fedora33_base.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./vm-image.sh \
 -b https://download.fedoraproject.org/pub/fedora/linux/releases/33/Cloud/x86_64/images/Fedora-Cloud-Base-33-1.2.x86_64.qcow2 \
--r quay.io/easybuild-vm \
--t f33-1.2
+-r quay.io/guimou/easybuild-vm \
+-t fc33-1.2
--- easybuild-vm/vm-image.sh
+++ easybuild-vm/vm-image.sh
@@ -42,32 +42,32 @@ fi
 
 set -x
 
-wget --continue $BASE_IMAGE -O /tmp/base_image.qcow2
+wget --continue $BASE_IMAGE -O base_image.qcow2
 
 if ! virt-customize --help >/dev/null; then
   sudo yum install -y virt-customize || sudo yum install -y libguestfs-tools-c
 fi
 
-virt-customize -a /tmp/base_image.qcow2 \
+virt-customize -a base_image.qcow2 \
   --run-command "adduser easybuild && echo \"easybuild:easybuild\" | chpasswd && usermod -aG wheel easybuild" \
   --install git,tar,which,bzip2,xz,make,automake,gcc,gcc-c++,patch,zlib-devel,openssl-devel,unzip,iproute,file,pam-devel,ant,sudo,lua,lua-devel,lua-posix,lua-filesystem,tcl,python-keyring \
-  --run-command "yum clean all" \
-  --run-command "rpm -ivh https://kojipkgs.fedoraproject.org/packages/http-parser/2.9.4/3.fc33/x86_64/http-parser-2.9.4-3.fc33.x86_64.rpm" \
-  --run-command "mkdir -p /build" \
-  --run-command "cd /build && curl -LO https://github.com/TACC/Lmod/archive/8.4.20.tar.gz && \
+  --run-command "yum clean all && \
+                rpm -ivh https://kojipkgs.fedoraproject.org/packages/http-parser/2.9.4/3.fc33/x86_64/http-parser-2.9.4-3.fc33.x86_64.rpm && \
+                mkdir -p /build && \
+                cd /build && curl -LO https://github.com/TACC/Lmod/archive/8.4.20.tar.gz && \
                 mv /build/8.4.20.tar.gz /build/Lmod-8.4.20.tar.gz && \
-                tar xvf Lmod-8.4.20.tar.gz" \
-  --run-command "cd /build/Lmod-8.4.20 && ./configure --prefix=/opt/apps --with-fastTCLInterp=no && \
-                  make install && \
-                  rm -rf /build && \
-                  ln -s /opt/apps/lmod/lmod/init/profile /etc/profile.d/z00_lmod.sh" \
+                tar xvf Lmod-8.4.20.tar.gz && \
+                cd /build/Lmod-8.4.20 && ./configure --prefix=/opt/apps --with-fastTCLInterp=no && \
+                make install && \
+                rm -rf /build && \
+                ln -s /opt/apps/lmod/lmod/init/profile /etc/profile.d/z00_lmod.sh" \
   --selinux-relabel \
 
-virt-sysprep -a /tmp/base_image.qcow2
+virt-sysprep -a base_image.qcow2
 
-cat <<END >/tmp/Dockerfile-$BASE
+cat <<END >Dockerfile
 FROM kubevirt/container-disk-v1alpha:v0.13.7
-ADD /tmp/base_image.qcow2 /disk/
+ADD base_image.qcow2 /disk/
 END
-docker build -t $REPO:$TAG /tmp/Dockerfile-$BASE_IMAGE
+docker build -t $REPO:$TAG .
 docker push $REPO:$TAG
