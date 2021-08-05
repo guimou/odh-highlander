147fee03aa930bfbc96c27156c8d45a6df10607e
Guillaume Moutier <guillaume.moutier@gmail.com>
2021-08-05 15:39:53 -0400

    Add usage and info

--- easybuild-container-ubi8/easybuild_install.sh
+++ easybuild-container-ubi8/easybuild_install.sh
@@ -4,5 +4,6 @@
 if ! command -v easybuild &> /dev/null
 then
     pip install --prefix /opt/apps/easybuild easybuild==4.4.1
+    mkdir -p /opt/apps/easybuild/easybuild.d
     cp /opt/apps/src/config.cfg /opt/apps/easybuild/easybuild.d/config.cfg
 fi
--- easybuild-container-ubi8/easybuild_run.sh
+++ easybuild-container-ubi8/easybuild_run.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+# Info on running container
+CONTAINER_NAME=`cat /etc/hostname`
+
+cat <<EOF
+Easybuild container is running with id ${CONTAINER_NAME}.
+
+You can connect to it locally by running:
+docker exec -it ${CONTAINER_NAME} bash -l
+
+You can kill it by running:
+docker kill ${CONTAINER_NAME}
+EOF
 
 # Always run hack
 /usr/bin/tail -f /dev/null
