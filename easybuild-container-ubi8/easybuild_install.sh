6eb9cfcd63aac211ab856cd23c5db73646359d91
Guillaume Moutier <guillaume.moutier@gmail.com>
2021-09-09 18:17:42 -0400

    ubi8-py39 working config

--- easybuild-container-ubi8/easybuild_install.sh
+++ easybuild-container-ubi8/easybuild_install.sh
@@ -4,7 +4,7 @@
 if ! command -v easybuild &> /dev/null
 then
     # Install EasyBuild
-    pip install --prefix /opt/apps/easybuild easybuild==4.4.1
+    pip install --prefix /opt/apps/easybuild easybuild==4.4.2
     # Create configuration file
     mkdir -p /opt/apps/easybuild/easybuild.d
     cp /opt/app-root/src/config.cfg /opt/apps/easybuild/easybuild.d/config.cfg
